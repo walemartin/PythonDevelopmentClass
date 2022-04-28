3a5d536390943751e107f49026878c53d26f763e
Olasupo Olawale Martin <rytesoft@hotmail.com>
2022-04-28 13:50:27 +0100

    range of indexes

--- PythonApplicaSolution.py
+++ PythonApplicaSolution.py
@@ -1,5 +1,15 @@
 
 #Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
 
+#There are four collection data types in the Python programming language: list,tuple,set and dictionary
+
 myList=["apple",'banana','cherry']
 print(len(myList))
+print(type(myList))
+
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list1 = ["abc", 34, True, 40, "male"]
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
